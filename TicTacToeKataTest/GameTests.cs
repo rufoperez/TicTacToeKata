8fca3e644b799285e503133235cb45cb61455dba
Rufino Perez <ruperez@aidacanarias.com>
2022-06-16 09:50:14 +0200

    red no_winner_when_player_X_plays_first

--- TicTacToeKataTest/GameTests.cs
+++ TicTacToeKataTest/GameTests.cs
@@ -5,19 +5,30 @@ namespace TicTacToeKataTest
 {
     public class GameTests
     {
+        private Game game;
+
         [SetUp]
         public void Setup()
         {
+            game = new Game();
         }
 
         [Test]
         public void error_when_player_O_plays_first()
         {
-            Game game = new Game();
             Action act = () => game.Play("O", 0, 0);
 
             act.Should().Throw<InvalidOperationException>()
                 .WithMessage("Player X must start the game");
         }
+
+        [Test]
+        public void no_winner_when_player_X_plays_first()
+        {
+            var winner = game.Play("X", 0, 0);
+
+            winner.Should().BeEmpty();
+        }
+
     }
 }
