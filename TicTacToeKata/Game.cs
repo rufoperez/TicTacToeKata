6b196265c95e5bfc5482c9dbb58a9afee89c7d6b
Rufino Perez <ruperez@aidacanarias.com>
2022-06-21 09:29:20 +0200

    green error_when_player_O_plays_twice and delete unused params

--- TicTacToeKata/Game.cs
+++ TicTacToeKata/Game.cs
@@ -4,7 +4,7 @@ public class Game
 {
     private string lastPlayer = string.Empty;
 
-    public void Play(string s, int x, int y)
+    public void Play(string s)
     {
         if (string.IsNullOrEmpty(lastPlayer) && !s.Equals("X"))
             throw new InvalidOperationException("Player X must start the game");
--- TicTacToeKataTest/GameTests.cs
+++ TicTacToeKataTest/GameTests.cs
@@ -16,7 +16,7 @@ namespace TicTacToeKataTest
         [Test]
         public void error_when_player_O_plays_first()
         {
-            Action act = () => game.Play("O", 0, 0);
+            Action act = () => game.Play("O");
 
             act.Should().Throw<InvalidOperationException>()
                 .WithMessage("Player X must start the game");
@@ -25,13 +25,23 @@ namespace TicTacToeKataTest
         [Test]
         public void error_when_player_X_plays_twice()
         {
-            game.Play("X", 0, 0);
+            game.Play("X");
 
-            Action act = () => game.Play("X", 0, 0);
+            Action act = () => game.Play("X");
 
             act.Should().Throw<InvalidOperationException>()
                 .WithMessage("Invalid player");
         }
 
+        [Test]
+        public void error_when_player_O_plays_twice()
+        {
+            game.Play("X");
+
+            Action act = () => game.Play("X");
+
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage("Invalid player");
+        }
     }
 }
