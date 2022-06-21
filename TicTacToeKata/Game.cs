2d406fae98039dc60a94953c639cadec66e6522c
Rufino Perez <ruperez@aidacanarias.com>
2022-06-21 09:26:27 +0200

    green error_when_player_X_plays_twice

--- TicTacToeKata/Game.cs
+++ TicTacToeKata/Game.cs
@@ -2,8 +2,15 @@ namespace TicTacToeKata;
 
 public class Game
 {
+    private string lastPlayer = string.Empty;
+
     public void Play(string s, int x, int y)
     {
-        throw new InvalidOperationException("Player X must start the game");
+        if (string.IsNullOrEmpty(lastPlayer) && !s.Equals("X"))
+            throw new InvalidOperationException("Player X must start the game");
+        if (lastPlayer.Equals(s))
+            throw new InvalidOperationException("Invalid player");
+        
+        lastPlayer = s;
     }
 }
